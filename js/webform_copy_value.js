f7c6fc59c5d156189bf92ea1a277e2a477334de5
root <nishant.bhorodia@gmail.com>
2017-06-30 14:38:23 +0530

    Added support for checkbox, radio and textarea fields

--- js/webform_copy_value.js
+++ js/webform_copy_value.js
@@ -5,11 +5,23 @@ jQuery(document).ready(function ($) {
   $("input[type=text]").focusout(function() {
     copyValueToTarget(this, sourceTargetMap);
   });
-  
+
+  $("textarea").focusout(function() {
+    copyValueToTarget(this, sourceTargetMap);
+  });
+
   $("select").change(function() {
     copyValueToTarget(this, sourceTargetMap);
   });
+
+  $("input[type=checkbox]").change(function() {
+    copyValueToTargetCheckbox(this, sourceTargetMap);
+  });
   
+  $("input[type=radio]").change(function() {
+    copyValueToTargetRadio(this, sourceTargetMap);
+  });
+
   function copyValueToTarget (fieldObj, sourceTargetMap) {
     var fieldID = fieldObj.id;
     var stArray = sourceTargetMap[fieldID];
@@ -17,5 +29,37 @@ jQuery(document).ready(function ($) {
     $.each(stArray, function (key, value) {
       $('[name="' + value + '"]').val($(fieldObj).val());
     });
+  }
+
+  function copyValueToTargetCheckbox (fieldObj, sourceTargetMap) {
+    var fieldID = fieldObj.id;
+
+    fieldObj = $(fieldObj);
+    var isChecked = fieldObj.is(':checked');
+    var checkboxVal = fieldObj.val();
+    var parentDivID = fieldID.replace('-' + checkboxVal, '');
+
+    if(sourceTargetMap[parentDivID]) {
+      var stArray = sourceTargetMap[parentDivID];
+      $.each(stArray, function (key, value) {
+        value += '[' + checkboxVal + ']';
+        $('[name="' + value + '"]').attr('checked', isChecked);
+      });
+    }
+  }
+
+  function copyValueToTargetRadio (fieldObj, sourceTargetMap) {
+    var fieldID = fieldObj.id;
+
+    fieldObj = $(fieldObj);
+    var radioVal = fieldObj.val();
+    var parentDivID = fieldID.replace('-' + radioVal, '');
+
+    if(sourceTargetMap[parentDivID]) {
+      var stArray = sourceTargetMap[parentDivID];
+      $.each(stArray, function (key, value) {
+        $('[name="' + value + '"]' + '[value=' + radioVal + ']').attr('checked', 'checked');
+      });
+    }
   }
 });
